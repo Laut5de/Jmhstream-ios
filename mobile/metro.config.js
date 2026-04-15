16c9bef0433e0550776d4208ac07388ab152c6d0
Laut5de <usaaajoker@gmail.com>
2026-04-15 12:12:06 -0400

    fix: revert metro.config.js - use only expo/metro-config (no @react-native/metro-config)

--- mobile/metro.config.js
+++ mobile/metro.config.js
@@ -1,6 +1,5 @@
 const { getDefaultConfig } = require('expo/metro-config');
-const { mergeConfig } = require('@react-native/metro-config');
 
-const defaultConfig = getDefaultConfig(__dirname);
+const config = getDefaultConfig(__dirname);
 
-module.exports = mergeConfig(defaultConfig, {});
+module.exports = config;
